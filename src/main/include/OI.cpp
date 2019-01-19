8a36c27cc893e80b353565411fb3ba122694d907
Murropoly <tylermuranaka@gmail.com>
2019-01-18 21:21:38 -0700

    I changed the formatting
    I tryed to make our code look cleanner and follow the microsoft format

--- src/main/include/OI.cpp
+++ src/main/include/OI.cpp
@@ -11,45 +11,56 @@
 //rather than typing trueMap(stick.GetRawAxis(number), 1.0, -1.0, 1.0, 0.0)
 //which takes forever
 
-double axis(int axisNumber) {
+double axis(int axisNumber)
+{
     return trueMap(stick.GetRawAxis(axisNumber), 1.0, -1.0, 1.0, 0.0);
 };
 
-bool intake() { //intake the ball
+bool intake()
+{ //intake the ball
     return stick.GetRawButton(1);
 }
 
-bool shooter() { //shoot the ball
+bool shooter()
+{ //shoot the ball
     return stick.GetRawButton(2);
 }
 
-bool armTop() { //arm to top level
+bool armTop()
+{ //arm to top level
     return stick.GetRawButton(3);
 }
 
-bool armMiddle() { //arm to middle level
+bool armMiddle()
+{ //arm to middle level
     return stick.GetRawButton(4);
 }
 
-bool armBottom() { //arm to bottom level
+bool armBottom()
+{ //arm to bottom level
     return stick.GetRawButton(5);
 }
 
-bool armReset() { //fully retract arm
+bool armReset()
+{ //fully retract arm
     return stick.GetRawButton(6);
 }
 
-bool panelUp() { //pick up the panel
+bool panelUp()
+{ //pick up the panel
     return stick.GetRawButton(7);
 }
 
-bool panelDown() { //place the panel
+bool panelDown()
+{ //place the panel
     return stick.GetRawButton(8);
 }
 
-bool armAlign() { //align the robot with the rocket
+bool armAlign()
+{ //align the robot with the rocket
     return stick.GetRawButton(9);
 }
 
-OI::OI() { //I don't really know why this is here
+OI::OI()
+{ //I don't really know why this is here
 }
